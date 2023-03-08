c56f7a5fa49017cf7e2ac6558c4520aa598eb117
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2023-03-08 16:04:38 -0300

    Enhance documentation

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,5 +1,11 @@
+<!-- https://vuejs.org/api/sfc-spec.html#sfc-syntax-specification -->
+
 <template>
   <div>
+    <!--
+      If the videoURL is not set, we'll hide the video (display: none).
+      https://vuejs.org/guide/essentials/class-and-style.html#binding-html-classes
+    -->
     <video
       ref="video"
       :style="{ display: videoURL ? 'block' : 'none' }"
@@ -7,8 +13,9 @@
       controls
       autoplay
     />
+    <!-- if the videoURL is not set, display a filepicker to the user -->
     <div v-if="!videoURL">
-      <input type="file" @change="loadVideo($event as any)" />
+      <input type="file" @change="loadVideo($event as InputChangeEvent)" />
     </div>
   </div>
 </template>
@@ -17,11 +24,15 @@
 import { useSubtitleStore } from "@/stores/subtitle";
 import { useVideoStore } from '@/stores/video';
 
+// Declares
 interface InputChangeEvent extends Event {
   target: HTMLInputElement;
 }
 
 export default {
+  // Setup the stores. The return of this function makes causes the
+  // stores to be available later via this.subStore and this.vidStore.
+  // https://vuejs.org/api/composition-api-setup.html#basic-usage
   setup() {
     const subStore = useSubtitleStore();
     const vidStore = useVideoStore();
@@ -31,13 +42,21 @@ export default {
       vidStore,
     };
   },
+
+  // "Declares" the videoURL variable. It's available later via
+  // this.videoURL.
+  // https://vuejs.org/api/options-state.html#data
   data() {
     return {
       videoURL: "",
     };
   },
+
+  // When this component is mounted, it registers to listen
+  // to the dancemat events.
+  // https://vuejs.org/api/composition-api-lifecycle.html#onmounted
   mounted() {
-    // TODO: Move them out of here
+    // TODO: Remove these and move them to another component
     this.$onDancematPress('lowerRight', () => {
       this.vidStore.advance(5);
     });
@@ -45,24 +64,30 @@ export default {
       this.vidStore.rewind(5);
     });
   },
+
+  // Methods this object has
+  // https://vuejs.org/api/options-state.html#methods
   methods: {
+    // This should be called when the user loads a video.
     loadVideo(event: InputChangeEvent) {
       if (event.target.files && event.target.files.length == 1) {
+        // Read the file chosen by the user
         const file = event.target.files[0];
-
         if (this.videoURL) {
           URL.revokeObjectURL(this.videoURL);
         }
         this.videoURL = URL.createObjectURL(file);
 
+        // Save the video element on the store
         const video = this.$refs.video as HTMLVideoElement;
         this.vidStore.setVideo(video);
 
+        // Create a subtitle track and save it on the subtitle store
         const subs = video.addTextTrack("subtitles", "Subtitle");
         this.subStore.setSubtitle(subs);
       } else {
         this.vidStore.setVideo(null);
-        this.videoURL = "";
+        this.videoURL = "";1
       }
     },
   },
@@ -70,6 +95,7 @@ export default {
 </script>
 
 <style scoped>
+/* https://vuejs.org/api/sfc-css-features.html#sfc-css-features */
 video {
   width: 100%;
   height: 100%;
--- src/stores/subtitle.ts
+++ src/stores/subtitle.ts
@@ -1,19 +1,31 @@
 import { ref, computed, type Ref } from "vue";
 import { defineStore } from "pinia";
 
+// https://pinia.vuejs.org/core-concepts/#setup-stores
 export const useSubtitleStore = defineStore("subtitle", () => {
+
   const subtitle: Ref<TextTrack> | Ref<null> = ref(null);
 
+  /**
+   * Returns all the cues in the subtitle
+   */
   function getCues() {
     return subtitle.value?.cues;
   }
   const cues = computed(getCues);
 
+  /**
+   * Returns the cue being displayed now
+   */
   function getActiveCue() {
     return subtitle.value?.activeCues ? subtitle.value.activeCues[0] : null;
   }
   const activeCue = computed(getActiveCue);
 
+  /**
+   * Defines the texttrack to be used by the store.
+   * @param s The text track to use
+   */
   function setSubtitle(s: TextTrack) {
     if (subtitle.value != null) {
       subtitle.value.mode = "disabled";
@@ -22,6 +34,12 @@ export const useSubtitleStore = defineStore("subtitle", () => {
     subtitle.value.mode = "showing";
   }
 
+  /**
+   * Adds a cue to the subtitle
+   * @param start The time the cue should start in seconds
+   * @param end The time the cue should end in seconds
+   * @param text The contents of cue
+   */
   function addCue(start: number, end: number, text: string) {
     if (subtitle.value) {
       const cue = new VTTCue(start, end, text);
@@ -29,6 +47,7 @@ export const useSubtitleStore = defineStore("subtitle", () => {
     }
   }
 
+  // The returned objects are the ones that will be exposed to be accessed via the store
   return {
     subtitle,
     cues,
