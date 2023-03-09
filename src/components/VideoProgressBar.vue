14472e5e86f6e1bda0cd4d31d03110010ee47371
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2023-03-09 17:35:32 -0300

    Refactor ProgressBar

---
+++ src/components/VideoProgressBar.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div v-if="vidStore.isVideoLoaded" class="slider-container">
+      <input ref="range" type="range" min="0" max="1" step="0.005" id="duracao" class="slider" @input="seek">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { useVideoStore } from '@/stores/video';
+
+export default {
+  setup() {
+    const vidStore = useVideoStore();
+
+    return {
+      vidStore,
+    };
+  },
+
+  computed: {
+    value() {
+      if (!this.vidStore.isVideoLoaded) {
+        return null;
+      }
+      return this.vidStore.currentTime / this.vidStore.duration;
+    }
+  },
+
+  methods: {
+    seek() {
+      const input = this.$refs.range as HTMLInputElement;
+      if (input == null || !this.vidStore.isVideoLoaded) {
+        return;
+      }
+
+      this.vidStore.setCurrentTime(this.vidStore.duration * Number(input.value));
+    }
+  },
+
+  watch: {
+    value() {
+      const input = this.$refs.range as HTMLInputElement;
+      if (input == null || this.value == null) {
+        return;
+      }
+
+      input.value = String(this.value);
+    }
+  },
+}
+</script>
+
+<style scoped>
+.slider-container {
+  width: 100%;
+}
+.slider-container > input {
+  width: 100%;
+}
+</style>
