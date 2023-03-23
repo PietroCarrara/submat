e328d27e13b68cb7c39506a0f4a24140be8df84f
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2023-03-23 20:23:20 -0300

    Tutorial on adding new cues

--- src/components/Tutorial.vue
+++ src/components/Tutorial.vue
@@ -47,6 +47,28 @@
                 </div>
               </div>
 
+              <div v-else-if="stage == 3" class="row">
+                <div class="col-md-5">
+                  <MatDiagram :pos-x="2" :pos-y="3" />
+                </div>
+                <div class="col-md-7">
+                  <p>
+                    Para marcar o início de uma fala, use a seta inferior. Aperte-a novamente
+                    para marcar o fim da fala. As zonas em azul marcam onde estão suas falas.
+                  </p>
+                  <video ref="video3" src="/video/tutorial.mp4" loop autoplay>
+                  </video>
+                  <div class="sub-box" ref="state3subbox">
+                    <div v-for="cue of stages[3].cues" class="sub-item" :style="{left: cue.start*100+'%', width: (cue.end-cue.start)*100+'%'}">
+                    </div>
+                    <div v-if="stages[3].start != -1" class="sub-item" :style="{left: stages[3].start*100+'%', width: (stages[3].progress-stages[3].start)*100+'%'}">
+                    </div>
+                    <div class="progress-tick" :style="{left: stages[3].progress*100+'%'}">
+                    </div>
+                  </div>
+                </div>
+              </div>
+
             </div>
           </div>
           <div class="modal-footer">
@@ -64,56 +86,131 @@ import { Modal } from "bootstrap";
 
 export default {
   data() {
-      return {
-          modal: null as Modal | null,
-          stage: 1,
-      };
+    return {
+      modal: null as Modal | null,
+      stage: 1,
+      stages: {
+        3: {
+          progress: 0,
+          cues: [] as {start: number, end: number}[],
+          start: -1,
+        }
+      }
+    };
   },
   mounted() {
-      this.modal = new Modal(this.$refs["modal-el"] as HTMLElement);
-      this.modal.show();
+    this.modal = new Modal(this.$refs["modal-el"] as HTMLElement);
+    this.modal.show();
+
+    this.$onDancematPress("down", () => this.onKeyPress("down"));
+    this.$onDancematPress("lowerLeft", () => this.onKeyPress("lowerLeft"));
+    this.$onDancematPress("lowerRight", () => this.onKeyPress("lowerRight"));
+  },
+  unmounted() {
+    // TODO: Deregister dancemat events
   },
   methods: {
-      async prev() {
-        if (this.stage > 1) {
-          // Preserve time between screens
-          let time = 0;
-          if (this.$refs['video' + this.stage]) {
-            time = (this.$refs['video' + this.stage] as HTMLVideoElement).currentTime;
-          }
-
-          this.stage--;
-          await this.$nextTick();
-
-          if (this.$refs['video' + this.stage]) {
-            (this.$refs['video' + this.stage] as HTMLVideoElement).currentTime = time;
-          }
-        }
-      },
-      async next() {
-        // Preserve time between screens
+    async prev() {
+      if (this.stage > 1) {
         let time = 0;
         if (this.$refs['video' + this.stage]) {
+          // Preserve time between screens
           time = (this.$refs['video' + this.stage] as HTMLVideoElement).currentTime;
         }
 
-        this.stage++;
+        this.onExitStage(this.stage);
+        this.stage--;
         await this.$nextTick();
 
         if (this.$refs['video' + this.stage]) {
+          // Preserve time between screens
           (this.$refs['video' + this.stage] as HTMLVideoElement).currentTime = time;
         }
+        this.onEnterStage(this.stage);
+      }
+    },
+    async next() {
+      let time = 0;
+      if (this.$refs['video' + this.stage]) {
+        // Preserve time between screens
+        time = (this.$refs['video' + this.stage] as HTMLVideoElement).currentTime;
+      }
 
-        if (this.stage >= 3) {
-            this.modal?.hide();
-        }
-      },
+      this.onExitStage(this.stage);
+      this.stage++;
+      await this.$nextTick();
+
+      if (this.$refs['video' + this.stage]) {
+        // Preserve time between screens
+        (this.$refs['video' + this.stage] as HTMLVideoElement).currentTime = time;
+      }
+
+      this.onEnterStage(this.stage);
+      if (this.stage >= 4) {
+          this.modal?.hide();
+      }
+    },
+    onKeyPress(key: string) {
+      switch (this.stage) {
+        case 3:
+          const video = this.$refs.video3 as HTMLVideoElement;
+          console.log(video);
+          if (key === "down") {
+            if (this.stages[3].start === -1) {
+              this.stages[3].start = video.currentTime / video.duration;
+            } else {
+              const end = video.currentTime / video.duration;
+              this.stages[3].cues.push({
+                start: this.stages[3].start,
+                end,
+              });
+              this.stages[3].start = -1;
+            }
+          }
+          break;
+      }
+    },
+    onEnterStage(stage: number) {
+      switch (stage) {
+        case 3:
+          const video = this.$refs.video3 as HTMLVideoElement;
+          video.addEventListener("timeupdate", () => {
+            this.stages[3].progress = video.currentTime / video.duration;
+          });
+          break;
+      }
+    },
+    onExitStage(stage: number) {
+
+    },
   },
+  watch: {
+    'this.$refs.video3.currentTime'(time: number) {
+      console.log(time);
+    }
+  }
 }
 </script>
 
 <style scoped>
-  video {
-    max-width: 100%;
-  }
+video {
+  max-width: 100%;
+}
+
+.sub-box {
+  height: 64px;
+  background-color: black;
+  overflow-x: hidden;
+}
+.sub-item {
+  height: 100%;
+  background-color: #2c3e50;
+  position: absolute;
+}
+.progress-tick {
+  position: absolute;
+  height: 100%;
+  width: 4px;
+  background-color: rgba(255, 255, 255, 0.75);
+}
 </style>
